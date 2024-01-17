9206bdbe225e90b10059b1a2c7659e77fed37d49
Blackchick15 <109077737+Blackchick15@users.noreply.github.com>
2024-01-17 20:47:22 +0200

    commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const joi = require('joi');
-const { join } = require('path');
+// const joi = require('joi');
+// const { join } = require('path');
 
-const paswordSchema = joi.string().min(3).max(10).alphanum();
-console.log(paswordSchema.validate);
+// const paswordSchema = joi.string().min(3).max(10).alphanum();
+// console.log(paswordSchema.validate);
+
+console.log(
+"Is Mangozedog.com a valid email?: ",
+validateEmail("Mangozedog.com"));
